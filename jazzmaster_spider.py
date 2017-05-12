068aa60a2f2af47df1dc66d15a3eb8d107ec30b3
Matheus Faustino <matheuspfaustino@gmail.com>
2017-05-12 23:27:53 +0200

    Check if the file exists before fetching (makes sense)

--- jazzmaster_spider.py
+++ jazzmaster_spider.py
@@ -31,29 +31,30 @@ class JazzmasterSpider(scrapy.Spider):
             request = scrapy.Request(title.css('iframe ::attr(src)').extract_first(), callback=self.iframeParse)
 
             # removes "/" => path error
-            request.meta['title'] = title.css('h2 ::text').extract_first().replace('/','-')
+            request.meta['title'] = title.css('h2 ::text').extract_first().replace('/', '-')
 
             self.logger.info('Fetching iFrame:' + request.meta['title'])
             yield request
 
     def iframeParse(self, response):
         filename = response.meta['title']
+        filename = os.path.join(DIR_OUTPUT, filename + '.mp3')
         url = response.xpath('normalize-space(/)').re_first(REGEX_URL_FILE)
 
-        self.logger.info('Fetching mp3 ' + url)
-        req = scrapy.Request(url, callback=self.savingMp3)
-        req.meta['filename'] = os.path.join(DIR_OUTPUT, filename + '.mp3')
+        if not os.path.exists(filename):
+            self.logger.info('Fetching mp3 ' + url)
+            req = scrapy.Request(url, callback=self.savingMp3)
+            req.meta['filename'] = filename
 
-        yield req
+            yield req
+        else:
+            self.logger.info('File already exists ' + filename)
 
     def savingMp3(self, response):
-        if not os.path.exists(response.meta['filename']):
-            self.logger.info('Saving mp3 ' + response.meta['filename'])
+        self.logger.info('Saving mp3 ' + response.meta['filename'])
 
-            with open(response.meta['filename'], 'wb') as f:
-                f.write(response.body)
-        else:
-            self.logger.info('File already exists ' + response.meta['filename'])
+        with open(response.meta['filename'], 'wb') as f:
+            f.write(response.body)
 
 
 process = CrawlerProcess()
